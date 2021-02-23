cbfcf34eabbb6abbc78bf1f630b8c498ed955a78
Fahrican Kaçan <fahrican.kcn@gmail.com>
2021-02-23 15:51:24 +0300

    Hello World

---
+++ StokTakip/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adres = "selcuklu mahallesi kolay sokak No:2 Ankara ";
+            Console.WriteLine(adres.Length);
+        }
+    }
+}
